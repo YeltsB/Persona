e031dff7ee5c23e289c09a4f835444cc7666f0a1
Yeltsin Baquedano <yeltsinbaquedano@gmail.com>
2021-09-23 19:45:50 -0600

    Agregar archivos de proyecto.

---
+++ Persona/Persona/MenuPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Persona.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Persona
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+
+        async void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtNombres.Text) & !string.IsNullOrEmpty(txtApellidos.Text) & !string.IsNullOrEmpty(txtCorreo.Text))
+            {
+                Personas pe = new Personas();
+
+                pe.Nombre = txtNombres.Text;
+                pe.Apellido = txtApellidos.Text;
+                pe.Edad = Convert.ToInt32(numEdad.Text);
+                pe.Correo = txtCorreo.Text;
+
+                var resultado = new SegundaPagina();
+                resultado.BindingContext = pe;
+                await Navigation.PushAsync(resultado);
+            }
+        }
+    }
+}
